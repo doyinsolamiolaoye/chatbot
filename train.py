f8fff7cddc045de8b60522dca327a44edbb172b9
Doyinsolami Olaoye <64823598+doyinsolamiolaoye@users.noreply.github.com>
2020-12-11 21:57:58 +0100

    Update train.py

--- train.py
+++ train.py
@@ -55,8 +55,7 @@ class ChatDataset(Dataset):
 
     # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-
+        return self.x_data[index]
 
 
 
